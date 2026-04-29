38d4c615d9199912bf98ee931d500449485cdc1a
--Darkness-- <patrickcortez2024@gmail.com>
2026-04-29 19:50:37 +0800

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,11 @@
+﻿# Automaton
+
+**Automaton** is a task orchestration tool similar to **Cron**
+but its going to be written in C#, which is its main difference from
+**Cron**. This project is written by *Cortez* using Visual Studio for 
+educational purposes.
+
+## License
+
+This project is under GNU General Public License V3, see the LICENSE file for more information.
+
---
+++ src/main.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Automaton: A simple task orchestration tool
+ * made by Cortez. Similar to crond in nature
+ * but written in C#.
+ */
+
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
+namespace Automaton
+{
+    public static class Automaton
+    {
+        static Action<string> print = (x) => { // Simple Anonymous Function for printing
+            Console.WriteLine(x);
+        };
+
+
+        public static void Main()
+        {
+            var time = new System.Timers.Timer // Our clock/timer
+            {
+                Interval = 1000,
+                Enabled = true
+            };
+
+            int secs = 0,limit = 20;
+
+            time.Elapsed += (s, e) => // simple function for the clock to increment our secs for counting how many seconds has elapsed
+            {
+                secs++;
+                print($"S: {secs}"); // logging how much time has passed
+            };
+            
+
+            time.Start();
+
+            while (true) { // when ijt starts we put a loop so it does not exit prematurely until the desired seconds is done.
+            
+                if(secs >= limit)
+                {
+                    time.Stop();
+                    break;
+                }
+
+            }
+
+            print($"--------------\nTotal Time Elapsed: {secs}"); // this is just a place holder (for now)
+            // this will be the entry point for this project
+
+            
+        }
+    }
+}
